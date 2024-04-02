2ef703e1a11867f36f21129ad3ea3e902d10f635
AnhDao19112001 <phantaanhdao@gmail.com>
2024-04-02 17:20:47 +0700

    Search and Sort Todo

--- be_composer/database/migrations/2024_04_02_040051_create_todo_table.php
+++ be_composer/database/migrations/2024_04_02_040051_create_todo_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('todo', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('name_todo');
             $table->string('time');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('todo');
+        Schema::dropIfExists('todos');
     }
 };
