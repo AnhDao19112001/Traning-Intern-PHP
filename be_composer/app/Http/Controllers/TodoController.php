eb0e80c613fc974ad471b44f6c7888923062bb98
AnhDao19112001 <phantaanhdao@gmail.com>
2024-04-02 11:42:38 +0700

    Laravel connect ReactJS

---
+++ be_composer/app/Http/Controllers/TodoController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todo = Todo::all();
+        return response()->json($todo);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $todo = new Todo([
+            'name_todo' => $request->get('name_todo'),
+            'time' => $request->get('time'),
+            'day' => $request->get('day'),
+            'description' => $request->get('description')
+        ]);
+        $todo->save();
+        return response()->json('Add Todo Success');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+    }
+}
--- be_composer/database/migrations/2024_04_02_040051_create_todo_table.php
+++ be_composer/database/migrations/2024_04_02_040051_create_todo_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('todo', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name_todo');
+            $table->string('time');
+            $table->string('day');
+            $table->longText('description');
+            $table->boolean('status')->default(false);
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('todo');
     }
 };
